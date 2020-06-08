beac6b7e49b91288ef2fd09cb77d51801d06a639
Jorge Piñas <jorgepinas@gmail.com>
2020-06-08 13:20:41 +0200

    My Project Setup Completed Successfully

---
+++ tfgecommercebackend/src/main/java/es/uax/ecommerce/tfgecommercebackend/App.java
@@ -0,0 +1,13 @@
+package es.uax.ecommerce.tfgecommercebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
